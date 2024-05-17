e6c85bbd8a364648771b51171a442e0b569fb2ad
Michael G <mike_gh@aim.com>
2024-05-17 21:19:40 +0300

    initial commit

---
+++ kilo.c
@@ -0,0 +1,27 @@
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILRNO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(){
+    tcgetattr(STDIN_FILENO, orig_termios);
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
+int main(){
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
+
+    return 0;
+}
