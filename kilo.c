17b47e8ae2d4895c157933400193b87b1eacaa00
Michael G <mike_gh@aim.com>
2024-05-17 21:33:43 +0300

    turning off canonical mode

--- kilo.c
+++ kilo.c
@@ -13,7 +13,7 @@ void enableRawMode(){
     atexit(disableRawMode);
     
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO);
+    raw.c_lflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -23,7 +23,6 @@ int main(){
 
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-
 
     return 0;
 }
