92997a71f0e69f97e6dcbfdd7313c04fa82846cf
Michael G <mike_gh@aim.com>
2024-05-23 15:50:02 +0300

    Last line tilde ~

--- kilo.c
+++ kilo.c
@@ -53,32 +53,31 @@ void enableRawMode(){
         die("tcsetattr");
 }
 
-int getCursorPosition(int *rows, int *cols){
-    if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4 ) return -1;
-    printf("\r\n");
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1) {
-        if(iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-    }
-    editorReadKey();
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+  return 0;
 }
 
-
-int getWindowsSize(int *rows, int *cols){
-    struct winsize ws;
-
-    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-        return getCursorPosition(rows, cols);
-    } else {
-        *cols = ws.ws_col;
-        *rows = ws.ws_row;
-        return 0;
-    }
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPosition(rows, cols);
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
 }
 
 char editorReadKey(){
@@ -104,11 +103,14 @@ void editorProcessKeypress() {
 
 /*** output ***/
 
-void editorDrawRows(){
-    int y;
-    for(y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~", 1);
+    if (y < E.screenrows - 1) {
+      write(STDOUT_FILENO, "\r\n", 2);
     }
+  }
 }
 
 void editorRefreshScreen(){
@@ -123,7 +125,7 @@ void editorRefreshScreen(){
 /*** init ***/
 
 void initEditor(){
-    if(getWindowsSize(&E.screenrows, &E.screencols) == -1) die("getWindowsSize");    
+    if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");    
 }
 
 int main(){
