b2ae3daa0be6aefff8a8795b6b92523d8c94da40
Michael G <mike_gh@aim.com>
2024-05-25 00:40:27 +0300

    The Page Up and Page Down keys

--- kilo.c
+++ kilo.c
@@ -16,7 +16,9 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,  //gets 1001
   ARROW_UP, //gets 1002
-  ARROW_DOWN //gets 1003
+  ARROW_DOWN, //gets 1003
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 
@@ -102,11 +104,21 @@ int editorReadKey(){
     if(read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
     if(seq[0] == '['){
-      switch(seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+      if(seq[1] >= '0' && seq[1] <= '9') {
+        if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if(seq[2] == '~'){
+          switch(seq[1]){
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      } else {      
+        switch(seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+        }
       }
     }  
     return '\x1b';
@@ -210,6 +222,13 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+        case PAGE_UP:
+        case PAGE_DOWN:
+          {
+            int times = E.screenrows;
+            while(times--) editorMoveCursor(c == PAGE_UP ? ARROW_UP: ARROW_DOWN);
+          }
+          break;
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
